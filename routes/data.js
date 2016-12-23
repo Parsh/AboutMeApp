8d420716c785e2b65400d2a9405bff8af77e950b
Parsh Jain <parsh.cosmos11@gmail.com>
2016-12-23 19:26:34 +0530

    All routes set

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,41 @@
 var express = require('express');
-var router = express.Router();
-var homeData = require('../json/homeData.json')
 
-/* GET users listing. */
+var homeData = require('../json/main-tile-data/homeData.json')
+var mlData = require('../json/main-tile-data/mlData.json')
+var webData = require('../json/main-tile-data/webData.json')
+
+var mlCardInfo = require('../json/cards-config-info/mlCardInfo.json')
+var webCardInfo = require('../json/cards-config-info/webCardInfo.json')
+var aboutCardInfo = require('../json/cards-config-info/aboutCardInfo.json')
+
+var router = express.Router();
+
+/* GET Main Covers Data */
 router.get('/homeData.json', function(req, res, next) {
     res.send(homeData);
+});
+
+router.get('/mlData.json', function(req, res, next) {
+    res.send(mlData);
+});
+
+router.get('/webData.json', function(req, res, next) {
+    res.send(webData);
+});
+
+
+/* GET Cards Config Data */
+
+router.get('/mlCardInfo.json', function(req, res, next) {
+    res.send(mlCardInfo);
+});
+
+router.get('/webCardInfo.json', function(req, res, next) {
+    res.send(webCardInfo);
+});
+
+router.get('/aboutCardInfo.json', function(req, res, next) {
+    res.send(aboutCardInfo);
 });
 
 module.exports = router;
